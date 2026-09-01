838d04ba9ccbe5aa9873c82c6e010e646abf79e0
NightRoadIx <quintanarxbio@gmail.com>
2026-09-01 15:49:01 -0600

    Create fentanilo01.c for aura farming
    Initial program to farm aura with variable declarations.

---
+++ fentanilo01.c
@@ -0,0 +1,35 @@
+/*
+    Programa inicial que sirve para farmear aura
+*/
+
+// STanDard Input Output
+#include <stdio.h>
+// STanDard LIBrary
+#include <stdlib.h>
+// STandard BOOLean
+#include <stdbool.h>
+
+// Función principal
+int main()
+{
+    // Variables en programación
+    // Lenguajes de programación
+    // fuertemente tipados
+    // Buleano (1 bit), requiere librería stdbool.h
+    bool valorBuleano = true; // false, 1 || 0
+    // Caracter (8 bits)
+    char valorCaracter = 'a';   // 64
+    // Entero corto (16 bits)
+    short variableCorta = 42;
+    // Entero (32 bits)
+    int valoresEnteros = 67;
+    // Entero largo (64 bits)
+    long variableLarga = 234567;
+
+    // Cadena de texto "entre comillas"
+    printf("A farmear aura\n");
+
+    return 0;
+}
+
+
