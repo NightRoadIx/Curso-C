f6ae1442ea1b30535f8ea636a1aee1da607a3200
NightRoadIx <quintanarxbio@gmail.com>
2026-09-02 17:04:14 -0600

    Enhance main function with new variables and input
    Added float and double variables, and user input functionality.

--- fentanilo01.c
+++ fentanilo01.c
@@ -12,6 +12,7 @@
 // Función principal
 int main()
 {
+    /* OJO, DE PREFERENCIA COLOCAR TODAS LAS VARIABLES AQUÍ */
     // Variables en programación
     // Lenguajes de programación
     // fuertemente tipados
@@ -26,10 +27,29 @@ int main()
     // Entero largo (64 bits)
     long variableLarga = 234567;
 
+    // Valor decimal (32 bits)
+    float valorFlotante = 3.1416;
+    // Valor decimal (64 bits)
+    double valorDoble = 3.1415926539;
+
+    float valorGrande = 4567890987;
+
+    int pelox;
+
+    /* LUEGO, YA COLOCAR TODO EL CÓDIGO */
     // Cadena de texto "entre comillas"
     printf("A farmear aura\n");
+    printf("El valor es: %d\n", valoresEnteros);
+    printf("El valor es: %x\n", valoresEnteros);
+    printf("El valor es: %c\n", valorCaracter);
+    printf("El valor es: %.2f\n", valorFlotante);
+    printf("El valor es: %lf\n", valorDoble);
+    printf("Valor con mantisa: %e\n", valorGrande);
+
+    // Ingresar datos
+    printf("Ingresa un entero: ");
+    scanf("%d", &pelox);
+    printf("El valor ingresado fue %d", pelox);
 
     return 0;
 }
-
-
